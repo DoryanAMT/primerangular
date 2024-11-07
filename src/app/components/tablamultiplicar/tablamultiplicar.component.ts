b90f2dbbc07d106c9c7a59c1fdbe47b3d6df5c3f
DoryanAlexandro <doryanamt@gmail.com>
2024-11-07 09:30:29 +0100

    07/11/2024 tabla completada

--- src/app/components/tablamultiplicar/tablamultiplicar.component.ts
+++ src/app/components/tablamultiplicar/tablamultiplicar.component.ts
@@ -6,16 +6,23 @@ import { Component, ViewChild, ElementRef  } from '@angular/core';
   styleUrl: './tablamultiplicar.component.css'
 })
 export class TablamultiplicarComponent {
-  @ViewChild("cajanumero") cajanumeroRef: ElementRef;
+  @ViewChild("cajanumero") cajanumeroRef!: ElementRef;
   public numeros:Array<number>
+  public numero: number
 
   constructor(){
     this.numeros = new Array<number>();
-    this.cajanumeroRef = new ElementRef(0)
+    this.numero = 0;
     
   }
   mostrarTabla(): void{
-    
+    this.numero = this.cajanumeroRef.nativeElement.value
+    let aux = new Array<number>();
+    for (let i = 1; i <= 10; i++) {
+      var resultado = this.numero * i
+      aux.push(resultado);
+    }
+    this.numeros = aux
   }
   
 }
